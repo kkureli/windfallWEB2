aadb568580a264a1de62e46bd836402ed1b20c9b
Kaan <kkureli@gmail.com>
2021-02-14 19:59:29 +0300

    Updates

---
+++ precache-manifest.3a8a8bfb317cc0994b798430670d4bee.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d1b7e2ac4940c27fd1127c34779fbf1c",
+    "url": "/windfallWEB2/index.html"
+  },
+  {
+    "revision": "ade8c5a2102e91d4706b",
+    "url": "/windfallWEB2/static/css/main.304a4f46.chunk.css"
+  },
+  {
+    "revision": "1e10bb8a8bb09d178955",
+    "url": "/windfallWEB2/static/js/2.891b56fe.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/windfallWEB2/static/js/2.891b56fe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ade8c5a2102e91d4706b",
+    "url": "/windfallWEB2/static/js/main.7386cff8.chunk.js"
+  },
+  {
+    "revision": "e0380b219c9e68233d85",
+    "url": "/windfallWEB2/static/js/runtime-main.46941bb7.js"
+  },
+  {
+    "revision": "df3f63ece9afb382cc30b5b723fbed2d",
+    "url": "/windfallWEB2/static/media/appSS.df3f63ec.png"
+  },
+  {
+    "revision": "62db69760cff5a565d428d3f595a55a5",
+    "url": "/windfallWEB2/static/media/app_logo.62db6976.png"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/windfallWEB2/static/media/logo.5d5d9eef.svg"
+  }
+]);
